15cc2f2041a5bc07488e7c99be481f91fec7c306
boolfalse <extravaganza1024@gmail.com>
2023-05-04 18:36:35 -0700

    nodemon, concurrently integration

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 
 import express from 'express';
+import products from "./products.js";
 const port = 5000;
 
 const app = express();
 
 app.get('/', (req, res) => {
     return res.send('API is running...');
+});
+app.get('/api/products', (req, res) => {
+    return res.json(products).status(200);
+});
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    return res.json(product || []).status(product ? 200 : 404);
 });
 
 app.listen(port, () => {
