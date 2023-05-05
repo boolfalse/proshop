f4112a2bbe6c3dd9090e05c8aaa3e12dd6334ff3
boolfalse <extravaganza1024@gmail.com>
2023-05-04 19:40:01 -0700

    mongoose basics

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 import products from "./products.js";
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from "./config/db.js";
+connectDB();
 const port = process.env.PORT || 5000;
 const app = express();
 
