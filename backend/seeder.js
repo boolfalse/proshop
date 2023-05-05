50323a1d0c4749798d006bf3027e7d8caedc43b8
boolfalse <extravaganza1024@gmail.com>
2023-05-05 13:45:23 -0700

    cleanup

--- backend/seeder.js
+++ backend/seeder.js
@@ -2,7 +2,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import users from './data/users.js';
-import products from "./products.js";
+import products from "./data/products.js";
 import User from './models/userModel.js';
 import Product from './models/productModel.js';
 import Order from './models/orderModel.js';
