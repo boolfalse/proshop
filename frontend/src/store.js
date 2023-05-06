cd72a013b82350b6763dbf1b9b1004bb689e72e8
boolfalse <extravaganza1024@gmail.com>
2023-05-06 02:04:21 -0700

    Cart Slice initialization

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,12 @@
 
 import { configureStore } from "@reduxjs/toolkit";
-import {apiSlice} from "./slices/apiSlice";
+import { apiSlice } from "./slices/apiSlice";
+import { cartSlice } from "./slices/cartSlice";
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
+        cart: cartSlice.reducer,
     },
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware().concat(apiSlice.middleware);
