4965ee9f446f442e587e7a8916896ff2c8cf8295
boolfalse <extravaganza1024@gmail.com>
2023-05-06 17:21:07 -0700

    Fix following error: No reducer provided for key "auth"

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
 import { cartSlice } from "./slices/cartSlice";
-import authSlice from "./slices/authSlice";
+import authReducer from "./slices/authSlice";
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         cart: cartSlice.reducer,
-        auth: authSlice.reducer,
+        auth: authReducer,
     },
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware().concat(apiSlice.middleware);
